efd87adfcfe303aa5951e8c4a1d899436dd0af56
Mark Florisson <mark@mlabs.city>
2019-10-31 16:00:13 +0000

    created a test file to check times.py if the output is correct

---
+++ 05-testing/test_times.py
@@ -0,0 +1,10 @@
+# This file runs a test of the functions in times.py
+import times
+
+def test_given_input():
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = times.overlap_time(large, short)
+    expected =[('2010-01-12 10:30:00', '2010-01-12 10:36:30'),
+               ('2010-01-12 10:37:30', '2010-01-12 10:44:00')]
+    assert result == expected
