3a4998e7488551fdae02ee32a7e1c09dfebeaeae
Mark Florisson <mark@mlabs.city>
2019-10-31 16:35:12 +0000

    created test for lecture times

---
+++ 05-testing/test_class_times.py
@@ -0,0 +1,21 @@
+# tests the file times.py for a different time range.
+import times
+
+def test_class_times():
+    break_length = 10*60
+    num_breaks = 2
+    lecture_intervals = num_breaks + 1
+    time_range_0 = times.time_range("2019-10-31 10:00:00",
+                                    "2019-10-31 13:00:00")
+
+    time_range_1 = times.time_range("2019-10-31 10:05:00",
+                                    "2019-10-31 12:55:00",
+                                    lecture_intervals,
+                                    break_length)
+
+    overlapppp = times.overlap_time(time_range_0, time_range_1)
+    expected = [('2019-10-31 10:05:00', '2019-10-31 10:55:00'),
+                 ('2019-10-31 11:05:00', '2019-10-31 11:55:00'),
+                 ('2019-10-31 12:05:00', '2019-10-31 12:55:00')]
+    assert overlapppp == expected
+test_class_times()
