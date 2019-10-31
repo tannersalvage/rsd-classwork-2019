ab89114dfc09589d813b9ebe5f02c3b391d66045
Mark Florisson <mark@mlabs.city>
2019-10-31 15:27:24 +0000

    added some comments and changed variable names

--- 05-testing/times.py
+++ 05-testing/times.py
@@ -1,22 +1,27 @@
 import datetime
 
-def time_range(t0, t1, n=1, g=0):
-    t0_s = datetime.datetime.strptime(t0, "%Y-%m-%d %H:%M:%S")
-    t1_s = datetime.datetime.strptime(t1, "%Y-%m-%d %H:%M:%S")
-    d = (t1_s - t0_s).total_seconds() / n + g * (1/n - 1)
-    sec_range = [(t0_s + datetime.timedelta(seconds=i * d + i * g),
-                  t0_s + datetime.timedelta(seconds=(i + 1) * d + i * g)) for i in range(n)]
-    return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
+def time_range(time0, time1, n=1, g=0):
+    # transform input times to the right format and to strings
+    time0_string = datetime.datetime.strptime(time0, "%Y-%m-%d %H:%M:%S")
+    time1_string = datetime.datetime.strptime(time1, "%Y-%m-%d %H:%M:%S")
+
+    # find the time in seconds between the dates
+    d = (time1_string - time0_string).total_seconds() / n + g * (1/n - 1)
+    sec_range = [(time0_string + datetime.timedelta(seconds=i * d + i * g),
+                  time0_string + datetime.timedelta(seconds=(i + 1) * d + i * g)
+                  ) for i in range(n)]
+    return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")
+            ) for ta, tb in sec_range]
 
 
-def overlap_time(obs1, obs2):
-    ot = []
-    for tr0, tr1 in obs1:
-        for tra, trb in obs2:
-            low = max(tr0, tra)
-            high = min(tr1, trb)
-            ot.append((low, high))
-    return ot
+def overlap_time(observation1, observation2):
+    overlap_time_array = []
+    for time_range0, time_range1 in observation1:
+        for time_range_a, time_range_b in observation2:
+            low = max(time_range0, time_range_a)
+            high = min(time_range1, time_range_b)
+            overlap_time_array.append((low, high))
+    return overlap_time_array
 
 
 if __name__ == "__main__":
